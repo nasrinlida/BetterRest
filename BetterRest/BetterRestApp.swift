0081b932a6f19b927882e8eab1c68d02281fc01e
nasrinlida <nasrinakterlida@gmail.com>
2021-04-23 22:58:35 +0600

    This is the initial commit of BetterRest app where I have used machine learning for the very first time in iOS.

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by nasrinlida on 27/2/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
